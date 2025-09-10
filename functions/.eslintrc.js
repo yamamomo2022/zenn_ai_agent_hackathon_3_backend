9297a0cc94caa1a050fb16922de92e334b7c7040
yamamomo <violetraisondetre@gmail.com>
2025-09-10 18:04:57 +0900

    feat:update lint

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    "indent": "off",
+    "object-curly-spacing": "off",
   },
 };
